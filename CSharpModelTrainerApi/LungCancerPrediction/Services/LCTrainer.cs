bde3bb8a50a051a0682cb4bbcc245ad3aed49226
omar <omarkurtovic19@gmail.com>
2026-06-17 14:00:26 +0200

    Updated seed to ensure equal starting point. Added testing methodology to word.

--- CSharpModelTrainerApi/LungCancerPrediction/Services/LCTrainer.cs
+++ CSharpModelTrainerApi/LungCancerPrediction/Services/LCTrainer.cs
@@ -42,6 +42,16 @@ namespace CSharpModelTrainerApi.LungCancerPrediction.Services
 
             Device defaultDevice = TrainingHelper.GetOptimalDevice();
             torch.set_default_device(defaultDevice);
+
+            long seed = 42; 
+            torch.manual_seed(seed);
+            if (torch.cuda.is_available())
+            {
+                torch.cuda.manual_seed(seed);
+                torch.cuda.manual_seed_all(seed);
+            }
+
+
             var dataDirectory = pathResolver.GetLungCancerDataPath();
 
             // 1. DATA LOADING BENCHMARK
